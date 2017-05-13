5b50f40a20ac0f07e4d3a7fde256a5eacb06b38f
aviel shaked <shaked.aviel@gmail.com>
2017-05-13 15:13:00 +0300

    add

---
+++ SeleniumSauceLab/Lab1.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumSauceLab
+{
+    [TestFixture("chrome", "45", "Windows 7", "", "")]
+    [TestFixture("chrome", "57", "Windows 10", "", "")]
+    [Parallelizable]
+    public class Lab1 : TestLab
+    {
+
+        public Lab1(string browser,string  version,string  os,string  deviceName, string deviceOrientation):base(browser,version,os,deviceName,deviceOrientation)
+        {
+
+        }
+       
+        [Test]
+        public void AppleTest()
+        {
+            driver.Navigate().GoToUrl("https://www.apple.com/");
+            StringAssert.Contains("Apple", driver.Title);
+            IWebElement query = driver.FindElement(By.ClassName("ac-gn-link-mac"));
+            query.Click();
+            Assert.True(driver.Url.Contains("Mac"),"not the right url");           
+        }
+
+        [Test]
+        public void googleTest2()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com");
+            StringAssert.Contains("Google", driver.Title);
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("Sauce Labs");
+            query.Submit();
+        }
+
+        [Test]
+        public void DownloadPDf()
+        {
+            driver.Url = "http://all-free-download.com/free-vector/download/wild-nature-background-fox-leaf-icons-repeating-design_6829632.html";
+            Thread.Sleep(2000);
+            driver.FindElement(By.XPath("//*[@id='detail_content']/div[2]/a")).Click();
+        }
+    }
+}
---
+++ SeleniumSauceLab/TestLab.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework.Interfaces;
+using System.IO;
+
+namespace SeleniumSauceLab
+{   
+    public class TestLab
+    {
+
+        internal static string userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        internal static string directory = Path.Combine(userProfile, "Downloads");
+
+        protected IWebDriver driver;
+        private String browser;
+        private String version;
+        private String os;
+        private String deviceName;
+        private String deviceOrientation;
+
+
+        public TestLab(String browser, String version, String os, String deviceName, String deviceOrientation)
+        {
+            this.browser = browser;
+            this.version = version;
+            this.os = os;
+            this.deviceName = deviceName;
+            this.deviceOrientation = deviceOrientation;
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddUserProfilePreference("download.default_directory", directory);
+
+            DesiredCapabilities caps = options.ToCapabilities() as DesiredCapabilities;
+            caps.SetCapability(CapabilityType.BrowserName, browser);
+            caps.SetCapability(CapabilityType.Version, version);
+            caps.SetCapability(CapabilityType.Platform, os);
+            caps.SetCapability("deviceName", deviceName);
+            caps.SetCapability("deviceOrientation", deviceOrientation);
+            caps.SetCapability("username", "aviel-shaked");
+            caps.SetCapability("accessKey", "2da32c14-a5e8-4a5f-8c81-f90804097bdc");
+            caps.SetCapability("name", TestContext.CurrentContext.Test.Name);
+            
+
+            driver = new RemoteWebDriver(new Uri("http://ondemand.saucelabs.com:80/wd/hub"), caps, TimeSpan.FromSeconds(600));
+            
+            
+            
+        }
+
+        //[SetUp]
+        //public void setup()
+        //{
+        //    driver = new ChromeDriver();
+        //}
+
+        [TearDown]
+        public void CleanUp()
+        {
+            bool passed = TestContext.CurrentContext.Result.Outcome.Status.Equals(TestStatus.Passed);
+            try
+            {
+                // Logs the result to Sauce Labs
+                ((IJavaScriptExecutor)driver).ExecuteScript("sauce:job-result=" + (passed ? "passed" : "failed"));
+            }
+            finally
+            {
+                // Terminates the remote webdriver session
+                driver.Quit();
+            }
+        }
+
+    }
+}
